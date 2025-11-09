035c8153cf1267a62ce0ab2a9d781d745d877a90
Ibrahim-Abozaid <ibrahimabozaid98@gmail.com>
2025-11-09 16:06:22 +0200

    Session02

---
+++ E Commerce.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using E_Commerce.services_Abstraction;
+using E_Commerce.Shared.DTOs.ProductDTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        //  Get : BaseUrl/api/Products
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProducts()
+        {
+            var Products = await _productService.GetAllProductsAsync();
+
+            return Ok(Products);
+        }
+
+        [HttpGet("{id}")]
+        //  Get : BaseUrl/api/Products/{id}
+        public async Task<ActionResult<ProductDTO>> GetProduct(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+
+            return Ok(product);
+        }
+
+        [HttpGet("brands")]
+        //  Get : BaseUrl/api/Products/brands
+        public async Task<ActionResult<IEnumerable<BrandDTO>>> GetAllBrands()
+        {
+            var Brands = await _productService.GetAllBrandsAsync();
+
+            return Ok(Brands);
+        }
+
+        [HttpGet("types")]
+        //  Get : BaseUrl/api/Products/types
+        public async Task<ActionResult<IEnumerable<TypeDTO>>> GetAllTypes()
+        {
+            var Types = await _productService.GetAllTypesAsync();
+            return Ok(Types);
+        }
+
+
+    }
+}
---
+++ ECommerceWeb/Extensions/WebApplicationRegisteration.cs
@@ -0,0 +1,33 @@
+﻿using E_Commerce.Domain.Contracts;
+using E_Commerce.Persistence.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ECommerceWeb.Extensions
+{
+    public static class WebApplicationRegisteration
+    {
+        public static async Task<WebApplication> MigrateDatabaseAsync(this WebApplication app)
+        {
+
+            await using var Scope = app.Services.CreateAsyncScope();
+
+            var DbContextService = Scope.ServiceProvider.GetRequiredService<StoreDbContext>();
+            var PendingMigrations = await DbContextService.Database.GetPendingMigrationsAsync();
+            if (PendingMigrations.Any())
+                await DbContextService.Database.MigrateAsync();
+
+            return app;
+
+        }
+        public static async Task<WebApplication> SeedDatabaseAsync(this WebApplication app)
+        {
+            await using var Scope = app.Services.CreateAsyncScope();
+            var dbContextService = Scope.ServiceProvider.GetRequiredService<StoreDbContext>();
+            var DataIntializerService = Scope.ServiceProvider.GetRequiredService<IDataIntializer>();
+            await DataIntializerService.IntializeAsync();
+
+            return app;
+        }
+    }
+}
