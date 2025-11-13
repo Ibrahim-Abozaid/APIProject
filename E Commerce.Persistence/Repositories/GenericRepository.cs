ab59d0ff3602dbdf30cabf8cb88e95b1b01a9664
Ibrahim-Abozaid <ibrahimabozaid98@gmail.com>
2025-11-13 23:11:58 +0200

    Session04

--- E Commerce.Persistence/Repositories/GenericRepository.cs
+++ E Commerce.Persistence/Repositories/GenericRepository.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,9 +21,81 @@ namespace E_Commerce.Persistence.Repositories
         }
         public async Task AddAsync(TEntity entity) => await _dbContext.Set<TEntity>().AddAsync(entity);
 
-        public async Task<IEnumerable<TEntity>> GetAllAsync() => await _dbContext.Set<TEntity>().ToListAsync();
+        public async Task<int> CountAsync(ISpecification<TEntity, TKey> specifications)
+        {
+            return await SpecificationEvaluater.CreateQuery(_dbContext.Set<TEntity>(), specifications).CountAsync();
+        }
+
+        #region GetAll Before using specification
+
+        /* public async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? condition = default,
+             List<Expression<Func<TEntity, object>>>? Includes = default)
+         {
+             if (condition is not null)
+             {
+                 return await _dbContext.Set<TEntity>().Where(condition).ToListAsync();
+
+             }
+             // _dbcontext.products ==> Entry point
+             if(Includes is not null)
+             {
+                 IQueryable<TEntity> EnteryPoint = _dbContext.Set<TEntity>();
+                 foreach(var includeExp in Includes)
+                 {
+                     EnteryPoint = EnteryPoint.Include(includeExp);
+                 }
+                 //_dbcontext.products
+                 //_dbcontext.products.Include(p => p.ProductBrand)
+                 //_dbcontext.products.Include(p => p.ProductBrand).Include(p => p.ProductType)
+                 return await EnteryPoint.ToListAsync();
+
+             }
+             return await _dbContext.Set<TEntity>().ToListAsync();
+         }*/
+
+
+        #endregion
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbContext.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(ISpecification<TEntity, TKey> specifications)
+        {
+            /* IQueryable<TEntity> EntryPoint = _dbContext.Set<TEntity>();
+             if (specifications.IncludeExpressions is not null)
+             {
+                 foreach (var includeExp in specifications.IncludeExpressions)
+                 {
+                     EntryPoint = EntryPoint.Include(includeExp);
+                 }
+             }
+             return await EntryPoint.ToListAsync();*/
+
+            #region using function instead
+
+            //var Query = SpecificationEvaluater.CreateQuery<TEntity, TKey>(_dbContext.Set<TEntity>(), specifications);
+
+            //return await Query.ToListAsync();
+
+
+            //return await SpecificationEvaluater.CreateQuery<TEntity, TKey>(_dbContext.Set<TEntity>(), specifications).ToListAsync();
+            // will take generic types from parameters :
+            return await SpecificationEvaluater.CreateQuery(_dbContext.Set<TEntity>(), specifications).ToListAsync();
+
+            #endregion
+
+
+
+        }
 
         public async Task<TEntity?> GetByIdAsync(TKey id) => await _dbContext.Set<TEntity>().FindAsync(id);
+
+        public async Task<TEntity?> GetByIdAsync(ISpecification<TEntity, TKey> specifications)
+        {
+            return await SpecificationEvaluater.CreateQuery(_dbContext.Set<TEntity>(), specifications).FirstOrDefaultAsync();
+        }
 
         public void Remove(TEntity entity) => _dbContext.Set<TEntity>().Remove(entity);
 
